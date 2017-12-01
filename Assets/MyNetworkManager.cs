b255b408d0f9c096c36499234813802d44188d48
Hrvoje <hrvoje.drinovac@gmail.com>
2017-12-01 02:33:01 +0100

    state after 292. Network Player Movement

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+	
+	public void MyStartHost() {
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host");
+		StartHost();
+	}
+
+	public override void OnStartHost (){
+		Debug.Log (Time.timeSinceLevelLoad + " Host Started");
+	}
+
+	public override void OnStartClient (NetworkClient myClient){
+		Debug.Log (Time.timeSinceLevelLoad + " Client Start Requested");
+		InvokeRepeating("ConnectionWait", 0.01f, 0.01f);
+	}
+
+
+	public override void OnClientConnect (NetworkConnection conn){
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+		CancelInvoke();
+	}
+
+
+	void ConnectionWait (){
+		Debug.Log (".");
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 move;
+	private Rigidbody m_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!isLocalPlayer) {
+			return;
+		}
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        move = (v*Vector3.forward + h*Vector3.right).normalized;
+		m_Rigidbody.AddForce(move);
+	}
+
+//    private void FixedUpdate()
+//    {
+//        // Call the Move function of the ball controller
+//		m_Rigidbody.AddForce(move);
+//    }
+}
